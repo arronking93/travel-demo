984202a4006d5ef7bf50f1a8ebc3827e00b097a0
arronking93 <sword9@126.com>
2020-06-09 01:52:47 +0800

    feat: swiper ok

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import fastClick from "fastclick";
 import "@/assets/styles/border.css";
 import "@/assets/styles/reset.css";
 import "@/assets/styles/iconfont.css";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+
+Vue.use(VueAwesomeSwiper);
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in list" :key="item.id">
+        <img class="swipe-img" :src="item.url" alt="" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeadSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      list: [
+        {
+          id: 1001,
+          url:
+            "//img1.qunarzz.com/sight/p0/2005/30/307fa37039eab5cba3.water.jpg_600x330_8db87481.jpg"
+        },
+        {
+          id: 1002,
+          url:
+            "//img1.qunarzz.com/sight/p0/1508/5a/9a1e5c492dec4945ff05f24025323a44.water.jpg_600x330_84ffcd8f.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper ::v-deep .swiper-pagination-bullet-active {
+  background: #fff;
+}
+.wrapper {
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  // overflow: hidden;
+  .swipe-img {
+    width: 100%;
+  }
+}
+</style>
