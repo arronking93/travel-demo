51284a8f6237eb492191a2f0416552c6bb634f67
arronking93 <sword9@126.com>
2020-06-08 23:19:54 +0800

    v: project init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "@/assets/styles/border.css";
+import "@/assets/styles/reset.css";
 
 Vue.config.productionTip = false;
 
